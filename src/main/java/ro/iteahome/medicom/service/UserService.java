0f9b78839ecd288b98d5421d909a2f15928db8e3
Marius Bistika <marius.bistika@gmail.com>
2020-07-05 01:58:35 +0300

    added consult service

--- src/main/java/ro/iteahome/medicom/service/UserService.java
+++ src/main/java/ro/iteahome/medicom/service/UserService.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,7 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ro.iteahome.medicom.config.rest.RestConfig;
+import ro.iteahome.medicom.exception.business.GlobalNotFoundException;
 import ro.iteahome.medicom.exception.business.NotNhsRegisteredException;
+import ro.iteahome.medicom.model.dto.ConsultDTO;
 import ro.iteahome.medicom.model.dto.UserRegistrationDTO;
 import ro.iteahome.medicom.model.entity.Role;
 import ro.iteahome.medicom.model.entity.User;
@@ -56,7 +59,7 @@ public class UserService implements UserDetailsService {
         return restTemplate.exchange(
                 restConfig.getSERVER_URL() + restConfig.getDOCTORS_URI() + "/existence/by-cnp/" + cnp,
                 HttpMethod.GET,
-                new HttpEntity<>(restConfig.buildAuthHeaders()),
+                new HttpEntity<>(restConfig.buildAuthHeaders(restConfig.getCREDENTIALS())),
                 Boolean.class).getBody();
     }
 
@@ -67,6 +70,31 @@ public class UserService implements UserDetailsService {
         } else {
             throw new NotNhsRegisteredException();
         }
+    }
+
+    private ConsultDTO findAllConsults(String cnp) {
+        ResponseEntity<ConsultDTO> consultDTOResponse =
+                restTemplate.exchange(
+                        restConfig.getSERVER_URL() + restConfig.getPATIENTS_URI() + "/find-consult/" + cnp,
+                        HttpMethod.GET,
+                        new HttpEntity<>(restConfig.buildAuthHeaders(restConfig.getCREDENTIALS())),
+                        ConsultDTO.class);
+        ConsultDTO consultDTO = consultDTOResponse.getBody();
+        if (consultDTO != null) {
+            return consultDTO;
+        } else {
+            throw new GlobalNotFoundException("Consult");
+        }
+    }
+
+    private ConsultDTO addConsult(ConsultDTO consultDTO) {
+        ResponseEntity<ConsultDTO> consultDTOResponseEntity =
+                restTemplate.exchange(
+                        restConfig.getSERVER_URL() + restConfig.getPATIENTS_URI() + "/add-consult",
+                        HttpMethod.POST,
+                        new HttpEntity<>(consultDTO, restConfig.buildAuthHeaders(restConfig.getCREDENTIALS())),
+                        ConsultDTO.class);
+        return consultDTOResponseEntity.getBody();
     }
 
 // OVERRIDDEN "UserDetailsService" METHODS: ----------------------------------------------------------------------------
